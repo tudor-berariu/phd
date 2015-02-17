e7590279a9c73c6f425f5852302a9dd864c51c7c
Tudor Berariu <tudor.berariu@gmail.com>
2015-02-17 17:34:21 +0200

    Added script to measure performance

---
+++ sos-rl/run_miners.jl
@@ -0,0 +1,18 @@
+using Miners
+
+function test(seasons, episodes)
+    const L = length(validActions);
+    for season in 1:seasons
+        gs = initialState();
+        ags = map(i->perceiveMap(gs, i), 1:AGENTS_NO);
+        for episode in 1:episodes
+            actions = [validActions[rand(1:L)] for a in 1:AGENTS_NO];
+            doActions(gs, actions);
+            ags = map((i,ag)->perceiveMap(gs, i, ag), 1:AGENTS_NO, ags);
+        end
+    end
+end
+
+test(10, 20);
+
+@time test(500, 1000);
